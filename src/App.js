0da5a8c9d2ccbba719e3b69675ed10a939354755
shrey jain <shreyjain009@gmail.com>
2022-02-20 04:09:19 +0530

    hello try

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import MainComponent from './components/main';
 
+
 function App() {
+  require('dotenv').config()
+
   const apiKey=process.env.REACT_APP_API_KEY
   return (
     <MainComponent apiKey={apiKey}/>
